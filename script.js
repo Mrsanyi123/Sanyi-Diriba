5ff693787572dfc2e689a246fc9883d174860cbb
Mrsanyi123 <160329857+Mrsanyi123@users.noreply.github.com>
2024-02-17 17:10:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+
+/*=================== toggle icon navbar ====================*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+    
+}
+/*=================== scroll sections active link ====================*/
+let sections = document.querySelectorAll('section')
+let navLinks = document.querySelectorAll('header nav a')
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+    /*=================== sticky navbar ====================*/
+ let header = document.querySelector ('header');
+
+ header.classList.toggle('sticky',window.scrollY > 100);
+
+ /*=================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+   menuIcon.classList.remove('bx-x');
+   navbar.classList.remove('active');
+    
+};
+
+/*=================== scroll reveal ====================*/
+ScrollReveal({
+     reset: true,
+     distance:'80px',
+     duration:2000,
+     delay:200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p , .about-content', { origin: 'left' });
+
+
+/*=================== typed js ====================*/
+  
+  const typed = new Typed('.multiple-text', {
+    strings: ['Fronted Developer','Tester','Full Stack Developer','Youtuber','TikToker','Player'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true   
+});
+const readMoreBtn = document.querySelector(".read-more-btn");
+const text = document.querySelector(".text");
+
+readMoreBtn.addEventListener("click", (e) => {
+  text.classList.toggle("show-more");
+  if (readMoreBtn.innerText === "Read More") {
+    readMoreBtn.innerText = "Read Less";
+  } else {
+    readMoreBtn.innerText = "Read More";
+  }
+});
+
+// *********************
+// This Code is for only the floating card in right bottom corner
+// **********************
+
+const touchButton = document.querySelector(".float-text");
+const card = document.querySelector(".float-card-info");
+const close = document.querySelector(".gg-close-r");
+
+touchButton.addEventListener("click", moveCard);
+close.addEventListener("click", moveCard);
+
+function moveCard() {
+  card.classList.toggle("active");
+}
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
